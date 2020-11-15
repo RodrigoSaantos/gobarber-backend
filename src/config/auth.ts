1b1ae63d651303b26f67144dabd8072f638650e3
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-11-15 14:06:22 -0300

    first commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '1167cd144b72bca777390e53ef12c8cd',
+    expiresIn: '1d',
+  },
+};
---
+++ src/middlewares/ensureAuthenticated.ts
@@ -0,0 +1,39 @@
+import { Request, Response, NextFunction } from 'express';
+import { verify } from 'jsonwebtoken';
+import AuthConfig from '../config/auth';
+import AppError from '../errors/AppError';
+
+interface TokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export default function ensureAuthenticated(
+  request: Request,
+  response: Response,
+  next: NextFunction,
+): void {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('JWT token is missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  const { secret } = AuthConfig.jwt;
+  try {
+    const decoded = verify(token, secret);
+
+    const { sub } = decoded as TokenPayload;
+
+    request.user = {
+      id: sub,
+    };
+
+    return next();
+  } catch {
+    throw new AppError('Invalid JWT token', 401);
+  }
+}
