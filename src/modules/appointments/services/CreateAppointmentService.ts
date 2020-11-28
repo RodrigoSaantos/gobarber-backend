a0a698c4525b91a8fce0c6e7bfc7a80d7d4823cd
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-11-28 00:28:04 -0300

    rewriting repositories

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -22,12 +22,10 @@ class CreateAppointmentService {
       throw new AppError('This appointment is alredy booked');
     }
 
-    const appointment = appointmentsRepository.create({
+    const appointment = await appointmentsRepository.create({
       provider_id,
       date: appointmentDate,
     });
-
-    await appointmentsRepository.save(appointment);
 
     return appointment;
   }
