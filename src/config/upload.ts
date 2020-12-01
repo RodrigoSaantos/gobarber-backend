a1a499db599d2fde8b11a4fbf8d778b46e730644
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-12-01 20:04:10 -0300

    Provider of storage

--- src/config/upload.ts
+++ src/config/upload.ts
@@ -2,13 +2,14 @@ import path from 'path';
 import crypto from 'crypto';
 import multer from 'multer';
 
-const tpmFolder = path.resolve(__dirname, '..', '..', 'tmp');
+const tmpFolder = path.resolve(__dirname, '..', '..', 'tmp');
 
 export default {
-  directory: tpmFolder,
+  tmpFolder,
+  uploadsFolder: path.resolve(tmpFolder, 'uploads'),
 
   storage: multer.diskStorage({
-    destination: tpmFolder,
+    destination: tmpFolder,
     filename(request, file, callback) {
       const fileHash = crypto.randomBytes(10).toString('hex');
       const fileName = `${fileHash}-${file.originalname}`;
