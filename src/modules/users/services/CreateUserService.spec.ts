b6670940658de12b47676a4e8f2321d308c3b397
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-12-05 20:13:38 -0300

    tests refactoring

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -3,15 +3,18 @@ import FakeHashProvider from '../providers/HashProvider/fakes/FakeHashProvider';
 import FakeUsersRepository from '../repositories/fakes/FakeUsersRepository';
 import CreateUserService from './CreateUserService';
 
-describe('CreateUser', () => {
-  it('Should be able to create a new user', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUsers = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
+let fakeUsersRepository: FakeUsersRepository;
+let fakeHashProvider: FakeHashProvider;
+let createUsers: CreateUserService;
 
+describe('CreateUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+    fakeHashProvider = new FakeHashProvider();
+    createUsers = new CreateUserService(fakeUsersRepository, fakeHashProvider);
+  });
+
+  it('Should be able to create a new user', async () => {
     const user = await createUsers.execute({
       name: 'Rodrigo',
       email: 'rodrigo@gmail.com',
@@ -22,13 +25,6 @@ describe('CreateUser', () => {
   });
 
   it('Should not be able to create a unew ser with same email from another', async () => {
-    const fakeUsersRepository = new FakeUsersRepository();
-    const fakeHashProvider = new FakeHashProvider();
-    const createUsers = new CreateUserService(
-      fakeUsersRepository,
-      fakeHashProvider,
-    );
-
     await createUsers.execute({
       name: 'Rodrigo',
       email: 'rodrigo@gmail.com',
