4cd9004342285da9007abf2ef20cb6cee42db53e
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-12-05 12:56:15 -0300

    saving tokens on database

---
+++ src/modules/users/infra/http/controllers/ResetPasswordController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+import SendForgotPasswordEmailService from '@modules/users/services/SendForgotPasswordEmailService';
+
+export default class ResetPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendForgotPasswodEmail = container.resolve(
+      SendForgotPasswordEmailService,
+    );
+
+    await sendForgotPasswodEmail.execute({
+      email,
+    });
+
+    return response.status(204).json();
+  }
+}
