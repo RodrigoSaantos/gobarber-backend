7a6cdb3a0f9585532da4507fd0f0774c04cf448e
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2020-12-19 19:44:09 -0300

    Express rate limit

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -3,17 +3,20 @@ import 'dotenv/config';
 
 import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import 'express-async-errors';
 
 import uploadConfig from '@config/upload';
 import AppError from '@shared/errors/AppError';
-import { errors } from 'celebrate';
+import rateLimiter from './middlewares/raterLimiter';
 import routes from './routes';
+
 import '@shared/infra/typeorm';
 import '@shared/container';
 
 const app = express();
 
+app.use(rateLimiter);
 app.use(cors());
 app.use(express.json());
 app.use('/file', express.static(uploadConfig.uploadsFolder));
