2d5b115682d228648929fefc992b38543d4d7960
rodrigosaantos <contato.rodrigosaantos@gmail.com>
2021-01-06 15:15:49 -0300

    Config babel

--- src/modules/appointments/services/ListProvidersService.ts
+++ src/modules/appointments/services/ListProvidersService.ts
@@ -28,8 +28,6 @@ class ListProvidersService {
         except_user_id: user_id,
       });
 
-      console.log(users);
-
       await this.cacheProvider.save(
         `providers-list:${user_id}`,
         classToClass(users),
